6d74ea9b9519a87fb911c7c0aa011516b4a20065
Marcell Vazquez-Chanlatte <mvc@linux.com>
2013-02-12 20:28:18 -0600

    Added fixed

--- manager.py
+++ manager.py
@@ -16,19 +16,37 @@ by: Marcell Vazquez-Chanlatte
 EPILOG = """or you know you could take this by hand..."""
 KEY_DSC = """Key that the password is stored under"""
 
+def load_keys(func):
+    def _load_keys(*args, **kwargs):
+        gpg = GPG(gnupghome=gpg_home)
+        with open(pass_loc, mode='rb') as f:
+            decoded = gpg.decrypt_file(f, passphrase=getpass())
+        if decoded.data == b'':
+            exit()
+        passes = loads(clean_gpg_json(decoded.data))
+        func(passes, *args)
+    return _load_keys
+
+
 def clean_gpg_json(data):
     return str(data)[2:-1].replace(r'\n', '\n')
+
+@load_keys
+def list_keys(passes, *args):
+    for key in passes.keys():
+        print(key)
+
+@load_keys
+def print_key(passes, key):
+    print(passes[key])
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=DESCR, epilog=EPILOG)
-    parser.add_argument('-k', dest='key', type=str, help=KEY_DSC, required=True)
+    parser.add_argument('-k', '--key', dest='key', type=str, help=KEY_DSC)
+    parser.add_argument('-l', '--list',  action='store_true')
     args = parser.parse_args()
-    gpg = GPG(gnupghome=gpg_home)
-    with open(pass_loc, mode='rb') as f:
-        decoded = gpg.decrypt_file(f, passphrase=getpass())
-    if decoded.data == b'':
-        print("Bad Password")
-        exit()
-    passes = loads(clean_gpg_json(decoded.data))
-    print(passes[args.key])
+    if args.list:
+        list_keys()
+    if args.key is not None:
+        print_key(args.key)
