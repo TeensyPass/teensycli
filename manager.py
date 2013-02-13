c3ec8a21c62416507a1ad89c91c0e4ba067cc50e
Marcell Vazquez-Chanlatte <mvc@linux.com>
2013-02-12 19:25:51 -0600

    Added argument parser

--- manager.py
+++ manager.py
@@ -1,18 +1,29 @@
-#!/usr/env python3
+#!/usr/bin/env python3
 
 from json import loads
 from getpass import getpass
 from os import environ
 from gnupg import GPG
+import argparse
 
 gpg_home = environ['GPG_HOME']
 pass_loc = environ['PASS_LOC']
+
+DESCR = """
+Password Manager
+by: Marcell Vazquez-Chanlatte
+"""
+EPILOG = """or you know you could take this by hand..."""
+KEY_DSC = """Key that the password is stored under"""
 
 def clean_gpg_json(data):
     return str(data)[2:-1].replace(r'\n', '\n')
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=DESCR, epilog=EPILOG)
+    parser.add_argument('-k', dest='key', type=str, help=KEY_DSC, required=True)
+    args = parser.parse_args()
     gpg = GPG(gnupghome=gpg_home)
     with open(pass_loc, mode='rb') as f:
         decoded = gpg.decrypt_file(f, passphrase=getpass())
@@ -20,4 +31,4 @@ if __name__ == '__main__':
         print("Bad Password")
         exit()
     passes = loads(clean_gpg_json(decoded.data))
-    print(passes['www.google.com'])
+    print(passes[args.key])
