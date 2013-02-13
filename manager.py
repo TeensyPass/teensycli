b508ae478cf021f5ca9d166a577194d5f62411de
Marcell Vazquez-Chanlatte <mvc@linux.com>
2013-02-12 22:54:06 -0600

    Added usernames and new arguments

--- manager.py
+++ manager.py
@@ -6,9 +6,6 @@ from os import environ
 from gnupg import GPG
 import argparse
 
-gpg_home = environ['GPG_HOME']
-pass_loc = environ['PASS_LOC']
-
 DESCR = """
 Password Manager
 by: Marcell Vazquez-Chanlatte
@@ -16,37 +13,58 @@ by: Marcell Vazquez-Chanlatte
 EPILOG = """or you know you could take this by hand..."""
 KEY_DSC = """Key that the password is stored under"""
 
-def load_keys(func):
-    def _load_keys(*args, **kwargs):
-        gpg = GPG(gnupghome=gpg_home)
-        with open(pass_loc, mode='rb') as f:
-            decoded = gpg.decrypt_file(f, passphrase=getpass())
-        if decoded.data == b'':
-            exit()
-        passes = loads(clean_gpg_json(decoded.data))
-        func(passes, *args)
-    return _load_keys
-
 
 def clean_gpg_json(data):
     return str(data)[2:-1].replace(r'\n', '\n')
 
-@load_keys
-def list_keys(passes, *args):
-    for key in passes.keys():
-        print(key)
 
-@load_keys
-def print_key(passes, key):
-    print(passes[key])
+def print_pass(passes, key):
+    try:
+        print(passes[key][1])
+    except KeyError:
+        print('Invalid Key')
+
+
+def print_user(passes, key):
+    try:
+        print(passes[key][0])
+    except KeyError:
+        print('Invalid Key')
+
+
+def get_env_vars():
+    try:
+        return environ['GPG_HOME'], environ['PASS_LOC']
+    except KeyError as e:
+        print('Environment variables not properly set: {0}'.format(e))
+        exit()
+
+
+def get_passes(gpg_home, pass_loc):
+    gpg = GPG(gnupghome=gpg_home)
+    with open(pass_loc, mode='rb') as f:
+        decoded = gpg.decrypt_file(f, passphrase=getpass())
+    if decoded.data == b'':
+        print("Incorrect Passphrase")
+        exit()
+    return loads(clean_gpg_json(decoded.data))
 
 
 if __name__ == '__main__':
+    get_env_vars()
     parser = argparse.ArgumentParser(description=DESCR, epilog=EPILOG)
     parser.add_argument('-k', '--key', dest='key', type=str, help=KEY_DSC)
     parser.add_argument('-l', '--list',  action='store_true')
+    parser.add_argument('-p', '--password',  action='store_true')
+    parser.add_argument('-u', '--user',  action='store_true')
     args = parser.parse_args()
+
+    passes = get_passes(*get_env_vars())
+
     if args.list:
-        list_keys()
+        for key in passes.keys(): print(key)
     if args.key is not None:
-        print_key(args.key)
+        if args.password:
+            print_pass(passes, args.key)
+        if args.user:
+            print_user(passes, args.key)
