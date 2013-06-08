8aee96fa4d98d5bdb35d9aa08f7a21e77dca1131
Marcell Vazquez-Chanlatte <mvc@linux.com>
2013-06-07 23:49:11 -0500

    Added setup.py and made it a proper distribution

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name='TeensyCli',
+    version='0.1.0',
+    author='Marcell Vazquez-Chanlatte, William Kennington III',
+    author_email='mvc@linux.com',
+    packages=find_packages(),
+    scripts=['scripts/teensy_lock.py'],
+    url='',
+    license='LICENSE.txt',
+    description='',
+    long_description=open('README.txt').read(),
+    requires = []
+)
